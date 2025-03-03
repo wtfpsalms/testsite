62c5833b2b92bbf1753c1b47a47fa130f7ee9376
unknown <gxdsins@gmail.com>
2025-03-03 17:18:02 -0600

    Add .gitignore, LICENSE, styles.css, script.js, and index.html

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('smiley-container').addEventListener('click', function() {
+    document.getElementById('menu').classList.toggle('hidden');
+    document.getElementById('smiley-container').style.display = 'none';
+});
